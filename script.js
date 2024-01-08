98d8520156d7a16f325959db0d848aa0eeebb893
Ida <idra0002@stud.kea.dk>
2024-01-08 23:46:54 +0100

    scroll to top knap

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ document.addEventListener("DOMContentLoaded", function () {
     navbarLinks.classList.toggle("active");
   });
 });
+
+// Show the button when scrolling down
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+// Scroll to the top when the button is clicked
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
